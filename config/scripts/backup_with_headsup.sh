ebd8a91294772ce025ac6c7c46483f1d2a872373
Cynthia <cynthia@cynthia.dev>
2024-02-29 16:45:52 +0100

    fix: proper use of tellraw

--- config/scripts/backup_with_headsup.sh
+++ config/scripts/backup_with_headsup.sh
@@ -6,35 +6,35 @@
 #! License, v. 2.0. If a copy of the MPL was not distributed with this
 #! file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
-mcrcon -p "$RCON_PASSWORD" "tellraw §4✿ §dCatgirl Village§f: Server restart in §u10 minutes§r"
+mcrcon -p "$RCON_PASSWORD" 'tellraw @a [{"text":"\u273f","color":"dark_red"},{"text":" Catgirl Village","color":"light_purple"},{"text":": Server restart in 10 minutes","color":"white"}]'
 sleep 5m
 
-mcrcon -p "$RCON_PASSWORD" "tellraw §4✿ §dCatgirl Village§f: Server restart in §u5 minutes§r"
+mcrcon -p "$RCON_PASSWORD" 'tellraw @a [{"text":"\u273f","color":"dark_red"},{"text":" Catgirl Village","color":"light_purple"},{"text":": Server restart in 5 minutes","color":"white"}]'
 sleep 4m
 
-mcrcon -p "$RCON_PASSWORD" "tellraw §4✿ §dCatgirl Village§f: §6Server restart in §u1 minute§r"
+mcrcon -p "$RCON_PASSWORD" 'tellraw @a [{"text":"\u273f","color":"dark_red"},{"text":" Catgirl Village","color":"light_purple"},{"text":": ","color":"white",}{"text":"Server restart in 1 minute","color":"gold"}]'
 sleep 30s
 
-mcrcon -p "$RCON_PASSWORD" "tellraw §4✿ §dCatgirl Village§f: §6Server restart in §u30 seconds§r"
+mcrcon -p "$RCON_PASSWORD" 'tellraw @a [{"text":"\u273f","color":"dark_red"},{"text":" Catgirl Village","color":"light_purple"},{"text":": ","color":"white",}{"text":"Server restart in 30 seconds","color":"gold"}]'
 sleep 20s
 
-mcrcon -p "$RCON_PASSWORD" "tellraw §4✿ §dCatgirl Village§f: §cServer restart in §u10 seconds§r"
+mcrcon -p "$RCON_PASSWORD" 'tellraw @a [{"text":"\u273f","color":"dark_red"},{"text":" Catgirl Village","color":"light_purple"},{"text":": ","color":"white",}{"text":"Server restart in 10 seconds","color":"red"}]'
 sleep 5s
 
-mcrcon -p "$RCON_PASSWORD" "tellraw §4✿ §dCatgirl Village§f: §cServer restart in §u5 seconds§r"
+mcrcon -p "$RCON_PASSWORD" 'tellraw @a [{"text":"\u273f","color":"dark_red"},{"text":" Catgirl Village","color":"light_purple"},{"text":": ","color":"white",}{"text":"Server restart in 5 seconds","color":"red"}]'
 sleep 1s
 
-mcrcon -p "$RCON_PASSWORD" "tellraw §4✿ §dCatgirl Village§f: §cServer restart in §u4 seconds§r"
+mcrcon -p "$RCON_PASSWORD" 'tellraw @a [{"text":"\u273f","color":"dark_red"},{"text":" Catgirl Village","color":"light_purple"},{"text":": ","color":"white",}{"text":"Server restart in 4 seconds","color":"red"}]'
 sleep 1s
 
-mcrcon -p "$RCON_PASSWORD" "tellraw §4✿ §dCatgirl Village§f: §cServer restart in §u3 seconds§r"
+mcrcon -p "$RCON_PASSWORD" 'tellraw @a [{"text":"\u273f","color":"dark_red"},{"text":" Catgirl Village","color":"light_purple"},{"text":": ","color":"white",}{"text":"Server restart in 3 seconds","color":"red"}]'
 sleep 1s
 
-mcrcon -p "$RCON_PASSWORD" "tellraw §4✿ §dCatgirl Village§f: §cServer restart in §u2 seconds§r"
+mcrcon -p "$RCON_PASSWORD" 'tellraw @a [{"text":"\u273f","color":"dark_red"},{"text":" Catgirl Village","color":"light_purple"},{"text":": ","color":"white",}{"text":"Server restart in 2 seconds","color":"red"}]'
 sleep 1s
 
-mcrcon -p "$RCON_PASSWORD" "tellraw §4✿ §dCatgirl Village§f: §cServer restart in §u1 second§r"
+mcrcon -p "$RCON_PASSWORD" 'tellraw @a [{"text":"\u273f","color":"dark_red"},{"text":" Catgirl Village","color":"light_purple"},{"text":": ","color":"white",}{"text":"Server restart in 1 second","color":"red"}]'
 sleep 1s
 
-mcrcon -p "$RCON_PASSWORD" "tellraw §4✿ §dCatgirl Village§f: §oLights off! See you on the other side"
+mcrcon -p "$RCON_PASSWORD" 'tellraw @a [{"text":"\u273f","color":"dark_red"},{"text":" Catgirl Village","color":"light_purple"},{"text":": Lights off! See you on the other side ","color":"white"},{"text":"uwu","obfuscated":true,"color":"white"}]'
 cv-backup
