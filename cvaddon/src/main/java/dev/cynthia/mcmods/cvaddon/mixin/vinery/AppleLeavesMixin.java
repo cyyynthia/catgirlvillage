53fe5c23656307ebddf94f85ad07375ad01b6b6c
Cynthia <cynthia@cynthia.dev>
2024-03-20 17:44:14 +0100

    chore: add missing license headers

--- cvaddon/src/main/java/dev/cynthia/mcmods/cvaddon/mixin/vinery/AppleLeavesMixin.java
+++ cvaddon/src/main/java/dev/cynthia/mcmods/cvaddon/mixin/vinery/AppleLeavesMixin.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) Cynthia Rey, All rights reserved.
+ * SPDX-License-Identifier: MPL-2.0
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
 package dev.cynthia.mcmods.cvaddon.mixin.vinery;
 
 import dev.cynthia.mcmods.cvaddon.CVAddonMod;
